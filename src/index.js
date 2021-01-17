c9c4ef1b5e24c317f05c982c3c8e5907274dedea
amysteel <75953169+amysteel@users.noreply.github.com>
2021-01-17 14:27:03 +0000

    Added axios, API from OpenWeather and js to display today's info

--- src/index.js
+++ src/index.js
@@ -1,1 +1,34 @@
+function getTemp(response) {
+    
+    let searchCityTempValue = Math.round(response.data.main.temp);
+    let searchCityTempElement = document.querySelector("#todays-temperature-value");
+    searchCityTempElement.innerHTML = `${searchCityTempValue}`;
+    console.log(`${apiUrl}&appid=${apiKey}`);
+    console.log(`${searchCityTempValue}`);
 
+    let searchCityNameValue = response.data.name;
+    let searchCityNameElement = document.querySelector("#city");
+    searchCityNameElement.innerHTML = `${searchCityNameValue}`;
+    console.log(`${searchCityNameValue}`);
+
+    let searchCityConditionsValue = response.data.weather[0].description;
+    let searchCityConditionsElement = document.querySelector("#conditions");
+    searchCityConditionsElement.innerHTML = `${searchCityConditionsValue}`;
+    console.log(`${searchCityConditionsValue}`);
+
+    let searchCityHumidityValue = response.data.main.humidity;
+    let searchCityHumidityElement = document.querySelector("#humidity");
+    searchCityHumidityElement.innerHTML = `${searchCityHumidityValue}`;
+    console.log(`${searchCityHumidityValue}`);
+
+    let searchCityWindSpeedValue = Math.round(response.data.wind.speed);
+    let searchCityWindSpeedElement = document.querySelector("#wind-speed");
+    searchCityWindSpeedElement.innerHTML = `${searchCityWindSpeedValue}`;
+    console.log(`${searchCityWindSpeedValue}`);
+}
+
+let apiKey = "4581002eb6d6effa90c6392584a38b4b";
+let units = "metric";
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=London&units=${units}`;
+
+axios.get(`${apiUrl}&appid=${apiKey}`).then(getTemp);
